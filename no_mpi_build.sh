06133a065c945e1022c043002c798d11d9bcb130
e91 <rusheniii@ornl.gov>
2020-12-16 12:09:43 -0500

    update install script 64-bit

--- no_mpi_build.sh
+++ no_mpi_build.sh
@@ -5,7 +5,7 @@ tar -xvf v3.12.5.tar.gz
 cd petsc-3.12.5
 export CFLAGS="-isystem $CONDA_PREFIX/include -O2 -fPIC"
 export FFLAGS="-isystem $CONDA_PREFIX/include -O2 -fPIC"
-./configure CFLAGS="$CFLAGS" FFLAGS="$FFLAGS" LDFLAGS="$LDFLAGS" CC=$GCC FC=$FC --with-mpi=0 --with-openmp=1 --with-openblas-dir="$CONDA_PREFIX" --prefix="$CONDA_PREFIX" --with-x=0
+./configure CFLAGS="$CFLAGS" FFLAGS="$FFLAGS" LDFLAGS="$LDFLAGS" CXX=$CXX CC=$GCC FC=$FC --with-mpi=0 --with-openmp=1 --with-openblas-dir="$CONDA_PREFIX" --prefix="$CONDA_PREFIX" --with-x=0 --download-sowing-cc=$GCC --download-sowing-cxx=$CXX --with-64-bit-indices=1
 LOCAL_PETSC_DIR="`cat configure.log | grep 'PETSC_DIR:' | cut -d':' -f 2 | xargs`"
 LOCAL_PETSC_ARCH="`cat configure.log | grep 'PETSC_ARCH:' | cut -d':' -f 2 | xargs`"
 make PETSC_DIR=$LOCAL_PETSC_DIR PETSC_ARCH=$LOCAL_PETSC_ARCH all
